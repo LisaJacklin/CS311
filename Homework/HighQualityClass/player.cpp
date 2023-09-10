918adf32621f56c2dc48427a7df52d7db7d53771
LisaJacklin <lmjacklin@alaska.edu>
2023-09-10 00:03:57 -0800

    entering default constructor and data members.

--- Homework/HighQualityClass/player.cpp
+++ Homework/HighQualityClass/player.cpp
@@ -5,3 +5,13 @@
 //Objective: High-Quality Class
 //CS 311 Data Structures and Analysis
 //Homework 1
+
+#include <player.hpp>
+//includes the player class created
+#include <doctest.h>
+//included for testing 
+
+int main() {
+
+
+}
--- Homework/HighQualityClass/player.hpp
+++ Homework/HighQualityClass/player.hpp
@@ -6,17 +6,37 @@
 //CS 311 Data Structures and Analysis
 //Homework 1
 
+#ifndef FILE_PLAYER_HPP_INCLUDED
+#define FILE_PLAYER_HPP_INCLUDED
+
+#include <string>
+//for std::string
+
 class Player {
-	
-	Player();
+public:
+	//note that copy constr, mov constr, copy assign, mov assign, and dstructor 
+	//are all automatically generated and need not be created.
 
-	Player& +operator();
-	Player& -operator();
+	//default constructor
+	Player() {
+		//set playername and playerusername to unknown,
+		//games set to zero
+	};
 
-	Player(const std::string & playerName){}
+	Player(const std::string& _playerName) {};
 
-	Player(const std::string & playerUsername) {}
+	Player(const std::string& _playerUsername) {};
 
-	Player(int& gamesPlayed) {}
+	Player(int& _gamesPlayed) {
+		//will need an assert because no nonnegative.
+	};
+
+	//Data members -----------------
+private:
+	std::string _playerName;
+	std::string _playerUsername;
+	int _gamesPlayed;
 
 }; //end of class Player
+
+#endif
