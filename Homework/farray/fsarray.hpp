73039782af4ef9ba422ed12baa511df0db23e47f
LisaJacklin <lmjacklin@alaska.edu>
2023-09-20 22:29:55 -0800

    three bool operators and a few adjustments

--- Homework/farray/fsarray.hpp
+++ Homework/farray/fsarray.hpp
@@ -5,15 +5,19 @@
 #ifndef FILE_FSARRAY_HPP_INCLUDED
 #define FILE_FSARRAY_HPP_INCLUDED
 
-template<>; //this is what will change the class below
+template<typename data_T>; //this is what will change the class below
 class FSArray {
+public:
+	//----------------------------------------------------
+	//public member types! used for variables later
+	using value_type = data_T;
+	using size_type = std::size_t;
 
 	//----------------------------------------------------
 	//Class Constructors
 	FSArray() {}; //this needs object of size 8.
 
-	~FSArray() {}; //frees any dynamically allocated memory
-
+	
 	//copy operators create entirely new copy of data, 
 	//modifying the copy doesn't change the original!
 
@@ -33,13 +37,20 @@ class FSArray {
 
 	//----------------------------------------------------
 	// Class Member Functions
-
-	void size() {}; //returns number of items in array
+	//returns number of items in array
+	size_type size() const {
+		return _size;
+		}; 
 	
-	void begin() {}; //return address of item 0 (think iterator)
+	data_T* begin() {}; //return address of item 0 (think iterator)
 
-	void end() {}; // returns address of item one past end of array (think iterator)
+	data_T* end() {}; // returns address of item one past end of array (think iterator)
 
+
+	//deconstructor
+	~FSArray() {
+		delete[] _data;
+	}; //frees any dynamically allocated memory
 
 	//----------------------------------------------------
 	//Class operators
@@ -49,18 +60,24 @@ class FSArray {
 
 	FSArray operator<();
 
-	FSArray operator<=();
+	FSArray operator<=(const FSarray& other) const {
+		return (*this < other) || (*this == other);
+	}
 
-	FSArray operator>();
+	FSArray operator>(const FSArray& other) const {
+		return !(*this <= other);
+	}
 
-	FSArray operator>=();
-
+	FSArray operator>=(const FSArray& other) const {
+		return !(*this < other);
+	}
 	//----------------------------------------------------
 	//Class Member Types
+private:
 
-	//value_type
+	size_type _size;//size_type
 
-	//size_type
+	data_T* _data;//size_type
 
 }; //end of class FSArray
 
