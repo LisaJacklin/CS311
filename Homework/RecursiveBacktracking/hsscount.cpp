22e861a9b5f69cb95bd8b1d7afa371e1acee657b
LisaJacklin <lmjacklin@alaska.edu>
2023-10-07 15:40:22 -0800

    commenting header

--- Homework/RecursiveBacktracking/hsscount.cpp
+++ Homework/RecursiveBacktracking/hsscount.cpp
@@ -2,3 +2,18 @@
 //Lisa Jacklin
 //10/2/2023
 
+#include "hsscount.hpp" //includes hsscount function
+
+
+int hssCount(int dim_x, int dim_y,
+             int hole_x, int hole_y,
+             int start_x, int start_y,
+             int finish_x, int finish_y) {
+
+
+}
+
+//hssCount has to call this function!
+//this function does recursive backtracking through hssCount to 
+//do much of the computational work.
+int hssCount_resurse(); 
--- Homework/RecursiveBacktracking/hsscount.hpp
+++ Homework/RecursiveBacktracking/hsscount.hpp
@@ -6,10 +6,15 @@
 #define HSSCOUNT_HPP_INCLUDED
 
 //when considering a board of n size. Note that we are using this to call 
-//hssCoutn_resurse
+//hssCount_resurse
 int hssCount(int dim_x, int dim_y,
-    int hole_x, int hole_y,
-    int start_x, int start_y,
-    int finish_x, int finish_y);
+             int hole_x, int hole_y,
+             int start_x, int start_y,
+             int finish_x, int finish_y);
+
+/*
+dim_x, dim_y:  dimensions of the board in x and y
+hole_x, hole_y: cordinates of the hole
+*/
 
 #endif // HSSCOUNT_HPP_INCLUDED
