9efbb04e40fa5674f71c52653a865c709f395897
LisaJacklin <lmjacklin@alaska.edu>
2023-10-07 15:20:58 -0800

    clearing .cpp to start again

--- Homework/RecursiveBacktracking/hsscount.hpp
+++ Homework/RecursiveBacktracking/hsscount.hpp
@@ -11,4 +11,8 @@ int hssCount(int dim_x, int dim_y,
     int start_x, int start_y,
     int finish_x, int finish_y);
 
+//inititing
+int hssCount_recurse(int dim_x, int dim_y, int hole_x, int hole_y,
+    int finish_x, int finish_y, int current_x, int current_y,
+    int visited);
 #endif // HSSCOUNT_HPP_INCLUDED
