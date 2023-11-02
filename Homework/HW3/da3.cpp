ec9f1e36b24d4c5858c55de9e2357ac54ae088b9
Lisa Jacklin <lmjacklin@alaska.edu>
2023-11-01 23:09:38 -0800

    HW 3 adjustments

--- Homework/HW3/da3.cpp
+++ Homework/HW3/da3.cpp
@@ -1,22 +1,56 @@
 // da3.cpp  writeup from SKELETON
 // Lisa Jacklin
 // 2023-09-24
+//Updated 2023-11-01
 //
 // For CS 311 Fall 2023
 // Source for Assignment 3 functions
 
 #include "da3.hpp"     // For Assignment 3 prototypes & templates
-
-#include <functional> //for std::function
+#include "llnode.hpp"
+#include <stdexcept>   // for std::out_of_range
+#include <functional>  //for std::function
 using std::function;
+
+
+//Exercise A
+template <typename ValueType>
+ValueType lookup(const LLNode<ValueType> * head, std::size_t index)
+{
+    //Remember, we need to throw exceptions
+    //so the first one to throw, is if the list is empty
+    if (head = nullptr) {
+        throw std::out_of_range("List is empty" + index);   
+    }
+
+    //now to check the value of the index
+    if (index == 0) 
+       {return head ->_data;} 
+  
+    const LLNode <ValueType>* current = head; //to determine how many to iterate
+    std::size_t counter = 0; //counter starts at zero
+
+    //trying a do loop for each count for size
+   while (current = current ->_next) {
+  
+        if (head == nullptr) {
+            throw std::out_of_range("lookup: invalid Index value!" + index);
+        }
+
+        current = current->_next;
+        ++counter;
+    }
+  
+}
 
 //Exercise B
 //this is to make sure that ff() throws
 void didItThrow(const function<void()> & ff, bool & threw)
 { 
+    threw = false;
+
     try {
         ff(); //takes no par!
-        threw = false;
     } //note ... as this is special!
     catch (...) {
         threw = true;
@@ -25,15 +59,39 @@ void didItThrow(const function<void()> & ff, bool & threw)
 
 }
 
+//Exercise C
+template <typename FDIter>
+bool checkSorted(FDIter first,FDIter last)
+{
+    if (first == last) {
+        return true; //empty range might as well be sorted
+  }
+
+    FDIter next = first;
+    ++next;
+
+    while (next != last) { // iterating so long as we aren't at the last item
+        if (*next < *first) {
+            return false;
+        }
+        ++first;
+        ++next;
+    }
+    return true;
+}
 //Exercise D
 //Preconditions: a and b and both not zero, 
 //               a and b are both positive
 int gcd(int a,int b)
 {
+    
+
     //note that this takes a / b to give the output
 
-    if (b == 0)  //part 1 when b is zero
+    if (b == 0)  //when b is zero
        {return a;} 
+    else if (a == 0 ) //and of course checking the other value as well
+        {return b;}
     else if (a > b) //need to check for if a is greater than b as well
         {return gcd(b, a);}
     else  //and lastly the general condition for gcd.
--- Homework/HW3/da3.hpp
+++ Homework/HW3/da3.hpp
@@ -14,36 +14,10 @@
 #include <functional>  // For std::function
 #include <stdexcept>   // for std::out_of_range
 
+//note that for simplification, i'm moving anything other than declarations to cpp
 //Exercise A
 template <typename ValueType>
-ValueType lookup(const LLNode<ValueType> * head, std::size_t index)
-{
-    //Remember, we need to throw exceptions
-    //so the first one to throw, is if the list is empty
-    if (!head) {
-        throw std::out_of_range("List is empty" + index);   
-    }
-
-    //now to check the value of the index
-    if (index == 0) 
-       {return head ->_data;} 
-  
-    const LLNode <ValueType>* current = head; //to determine how many to iterate
-    std::size_t counter = 0; //counter starts at zero
-
-    //trying a do loop for each count for size
-   do counter++  
-    {
-        //need to check for null here!
-        if (counter == nullptr) {
-            throw std::out_of_range("lookup: invalid Index value!" + index);
-        }
-
-        current = current->_next;
-        ++counter;
-    } while (current = current ->_next);
-  
-}
+ValueType lookup(const LLNode<ValueType> * head, std::size_t index);
 
 //Exercise B
 // Implementation in source file
@@ -51,30 +25,12 @@ void didItThrow(const std::function<void()> & ff,bool & threw);
 
 //Exercise C
 template <typename FDIter>
-bool checkSorted(FDIter first,FDIter last)
-{
-    if (first == last) {
-        return true; //empty range might as well be sorted
-  }
-
-    FDIter next = first;
-    ++next;
-
-    while (next != last) { // iterating so long as we aren't at the last item
-        if (*next < *first) {
-            return false;
-        }
-        ++first;
-        ++next;
-    }
-    return true;
-}
+bool checkSorted(FDIter first,FDIter last);
 
 //Exercise D
 // Implementation in source file
 //note that this is here to calculate the greatest common denominators
 int gcd(int a,int b);
-
 
 #endif  //#ifndef FILE_DA3_HPP_INCLUDED
 
